b41f27a271ef9ff07afc58510dd321dba7eab57a
Shivani Puttaraju <97019847+Shivaniputtaraju@users.noreply.github.com>
2022-03-11 04:25:37 +0000

    p1original.c

---
+++ p4original.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int input_degree()
+{
+  int n;
+  printf("enter the degree of the polynomial\n");
+  scanf("%d",&n);
+  return n;
+}
+float input_x()
+{
+  float x;
+  printf("enter the value of x\n");
+  scanf("%f",&x);
+  return x;
+}
+void input_coefficients(int n,float a[n])
+{
+  int i;
+  
+}
